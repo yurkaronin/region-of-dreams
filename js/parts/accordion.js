2646135b322e89dec9c87f51bc2906071dfe048b
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-09-12 09:41:48 +0300

    доработки аккордеона

--- js/parts/accordion.js
+++ js/parts/accordion.js
@@ -1,16 +1,28 @@
-document.querySelectorAll('.accordion__header').forEach(header => {
-  header.addEventListener('click', () => {
-    const item = header.parentElement;
-    const isActive = item.classList.contains('accordion__item_active');
+document.addEventListener('DOMContentLoaded', () => {
+  // Находим первый аккордеон и делаем его активным
+  const firstAccordionItem = document.querySelector('.accordion__item');
+  if (firstAccordionItem) {
+    firstAccordionItem.classList.add('isActive');
+  }
 
-    // Закрыть все аккордеоны
-    document.querySelectorAll('.accordion__item').forEach(i => {
-      i.classList.remove('accordion__item_active');
+  // Добавляем обработчики событий для всех заголовков аккордеонов
+  document.querySelectorAll('.accordion__header').forEach(header => {
+    header.addEventListener('click', () => {
+      const item = header.parentElement.parentElement; // Получаем родительский элемент .accordion__item
+      const isActive = item.classList.contains('isActive');
+
+      // Если текущий аккордеон уже активен, просто удаляем класс активности
+      if (isActive) {
+        item.classList.remove('isActive');
+      } else {
+        // Закрыть все аккордеоны
+        document.querySelectorAll('.accordion__item').forEach(i => {
+          i.classList.remove('isActive');
+        });
+
+        // Добавить класс активности к текущему аккордеону
+        item.classList.add('isActive');
+      }
     });
-
-    // Открыть текущий аккордеон, если он не был активен
-    if (!isActive) {
-      item.classList.add('accordion__item_active');
-    }
   });
 });
