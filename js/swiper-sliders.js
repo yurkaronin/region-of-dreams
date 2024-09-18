ce656c6ad7a036397eb51391e0dd933a5b194100
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-09-18 14:46:53 +0300

    обновил вёрстку - добавил вторую модалку

--- js/swiper-sliders.js
+++ js/swiper-sliders.js
@@ -1,9 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  if (document.querySelector(".js-slider-1")) {
+  // Слайдеры в первом экране
+  if (document.querySelector(".hero")) {
+    var heroPhotoSlider = new Swiper(".js-hero-photo-slider", {
+      loop: true,
+      spaceBetween: 0,
+      slidesPerView: 1,
+      // effect: "fade",
+    });
 
+    var heroTextSlider = new Swiper(".js-hero-text-slider", {
+      loop: true,
+      spaceBetween: 32,
+      slidesPerView: 1,
+      autoHeight: true,
+      navigation: {
+        nextEl: ".hero .swiper-button-next",
+        prevEl: ".hero .swiper-button-prev",
+      },
+      // pagination: {
+      //   el: ".hero .swiper-pagination",
+      //   clickable: true,
+      // },
+    });
+
+    // Синхронизация слайдеров
+    heroTextSlider.on("slideChange", function () {
+      var currentIndex = heroTextSlider.realIndex;
+      heroPhotoSlider.slideToLoop(currentIndex);
+    });
+  }
+
+  if (document.querySelector(".js-slider-1")) {
     var swiper = new Swiper(".js-slider-1 .swiper", {
-      slidesPerView: 'auto', // Количество слайдов определяется автоматически
+      slidesPerView: "auto", // Количество слайдов определяется автоматически
       spaceBetween: 32, // Отступы между слайдами
       loop: true,
       autoHeight: true,
@@ -14,18 +44,14 @@ document.addEventListener("DOMContentLoaded", function () {
       on: {
         resize: function () {
           swiper.update(); // Обновляем Swiper при изменении размера окна
-        }
-      }
+        },
+      },
     });
-
-
   }
 
-
   if (document.querySelector(".js-slider-2")) {
-
     var swiper = new Swiper(".js-slider-2 .swiper", {
-      slidesPerView: 'auto', // Количество слайдов определяется автоматически
+      slidesPerView: "auto", // Количество слайдов определяется автоматически
       spaceBetween: 32, // Отступы между слайдами
       loop: true,
       autoHeight: true,
@@ -36,12 +62,34 @@ document.addEventListener("DOMContentLoaded", function () {
       on: {
         resize: function () {
           swiper.update(); // Обновляем Swiper при изменении размера окна
-        }
-      }
+        },
+      },
     });
-
-
   }
 
-
+  if (document.querySelector(".js-slider-3")) {
+    var swiper = new Swiper(".js-slider-3 .swiper", {
+      slidesPerView: 1, // Количество слайдов определяется автоматически
+      spaceBetween: 32, // Отступы между слайдами
+      loop: true,
+      autoHeight: true,
+      navigation: {
+        nextEl: ".js-slider-3 .swiper-button-next",
+        prevEl: ".js-slider-3 .swiper-button-prev",
+      },
+      on: {
+        resize: function () {
+          swiper.update(); // Обновляем Swiper при изменении размера окна
+        },
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: "auto",
+        },
+      },
+    });
+  }
 });
